bc869f4f86134eed278065829ce11849f6f938bf
patricktelnoni <ptelnoni89@gmail.com>
2019-03-12 12:27:13 +0700

    And now, i can get text without using html parser. Time to deploy

--- dataset/spiders/kompas_spider.py
+++ dataset/spiders/kompas_spider.py
@@ -1,19 +1,19 @@
 import scrapy, csv
 from dataset.items import DatasetItem
-from html.parser import HTMLParser
+# from html.parser import HTMLParser
 
 
-class MLStripper(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.reset()
-        self.strict = False
-        self.convert_charrefs= True
-        self.fed = []
-    def handle_data(self, d):
-        self.fed.append(d)
-    def get_data(self):
-        return ''.join(self.fed)
+# class MLStripper(HTMLParser):
+#     def __init__(self):
+#         super().__init__()
+#         self.reset()
+#         self.strict = False
+#         self.convert_charrefs= True
+#         self.fed = []
+#     def handle_data(self, d):
+#         self.fed.append(d)
+#     def get_data(self):
+#         return ''.join(self.fed)
 
 
 
@@ -30,23 +30,24 @@ class KompasSpider(scrapy.Spider):
         # for url in urls:
         #     yield scrapy.Request(url=url, callback=self.parse)
 
-    def strip_tags(self, html):
-        s = MLStripper()
-        s.feed(html)
-        return s.get_data()
+    # def strip_tags(self, html):
+    #     s = MLStripper()
+    #     s.feed(html)
+    #     return s.get_data()
 
     def parse_content(self, response):
-        doc = DatasetItem()
+        doc         = DatasetItem()
+        category    = response.request.url.split("/")[2].split(".")[0]
 
         with open('datasetkompas.csv', 'a') as f:
             writer = csv.writer(f)
             judul           = response.xpath('//h1[@class="read__title"]/text()').extract()
-            cleaned_article = [self.strip_tags(p) for p in response.xpath('//div[@class="read__content"]/descendant::*').extract()]
+            cleaned_article = [p for p in response.xpath('//div[@class="read__content"]/descendant::text()').extract()]
             doc['title']    = judul
             doc['content']  = cleaned_article
             data = [
-                    self.id,
-                    'olahraga',
+                    # self.id,
+                    category,
                     doc['title'],
                     doc['content']
             ]
@@ -63,15 +64,15 @@ class KompasSpider(scrapy.Spider):
             url = link.xpath('//a/@href').extract()
             for u in url:
                 if len(str(u)) > 50:
-                    yield {"url": u}
-                    # yield scrapy.Request(u, callback=self.parse_content)
+                    # yield {"url": u}
+                    yield scrapy.Request(u, callback=self.parse_content)
 
     def parse(self, response):
         for link in response.css('.nav__wrap'):
             url = link.xpath('//li[@class="nav__item"]/a/@href').extract()
             for u in url:
-                yield {"url": u}
-                    # yield scrapy.Request(u, callback=self.parse_content)
+                # yield {"url": u}
+                yield scrapy.Request(u, callback=self.parsemenu)
 
 
 
